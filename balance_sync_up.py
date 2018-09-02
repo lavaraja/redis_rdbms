51c127394382b858e98cc9698d7652d83bcb0cc2
lavaraja <lavaraja.padala@gmail.com>
2018-09-02 23:34:02 +0530

    still under development

---
+++ balance_sync_up.py
@@ -0,0 +1,19 @@
+from local_settings import *
+import redis,psycopg2
+
+conn=redis.StrictRedis(host=RDB_HOST,port=RDB_PORT,db=RDB_DB)
+db_conn=psycopg2.connect(database=DB_NAME,user=DB_USER,password=DB_PASSWORD,host=DB_HOST,port=DB_PORT)
+
+def sync_balance(id):
+    cursor=db_conn.cursor()
+    print(conn.hmget('user:balance',id))
+    print(id)
+    db_conn.close()
+    #code
+
+while True:
+    sync_up_list=conn.zrangebyscore('delayset',10,50)
+    for x in sync_up_list:
+        sync_balance(x)
+
+
